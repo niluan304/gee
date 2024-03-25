d515fbf941b759edeca8a48f4259af8eaf89da4d
Zwei <zwei.elen@outlook.com>
2024-03-25 18:05:26 +0800

    fix(gee-web-day10): format code

--- web/day10/handle/handle_test.go
+++ web/day10/handle/handle_test.go
@@ -24,6 +24,7 @@ func TestObjectHandler(t *testing.T) {
 	defer server.ListenAndServe()
 
 	go func() {
+		// 等待路由注册
 		time.Sleep(time.Second * 2)
 
 		http.Get("http://localhost:8080/gf/hello-world")
@@ -32,36 +33,36 @@ func TestObjectHandler(t *testing.T) {
 		http.Get("http://localhost:8080/iris/hello/world")
 		http.Post("http://localhost:8080/iris/hello/world", "application/json", nil)
 
+		// 等待打印请求响应
 		time.Sleep(time.Second * 2)
 		server.Shutdown(context.TODO())
 	}()
 
+	// 根据结构体的 tag 注册路由
 	gf := r.Group("/gf")
-	iris := r.Group("/iris")
-
 	handle.ObjectHandler(Hello{}, func(f *handle.ReqResFunc, methodName string) {
-		handler := f.Handler()
-		{
-			field, find := f.Req().Elem().FieldByName("meta")
-			if !find {
-				panic("req must be contain meta filed")
-			}
+		field, find := f.Req().Elem().FieldByName("meta")
+		if !find {
+			panic("req must be contain meta filed")
+		}
 
-			tag := field.Tag
-			gf.Handle(tag.Get("method"), tag.Get("path"), handler)
-		}
-		{
-			var name []rune
-			for _, r := range methodName {
-				if unicode.IsUpper(r) {
-					name = append(name, '/')
-				}
-				name = append(name, unicode.ToLower(r))
+		tag := field.Tag
+		gf.Handle(strings.ToUpper(tag.Get("method")), tag.Get("path"), f.Handler())
+	})
+
+	// 根据方法名注册路由
+	iris := r.Group("/iris")
+	handle.ObjectHandler(Hello{}, func(f *handle.ReqResFunc, methodName string) {
+		var name []rune
+		for _, r := range methodName {
+			if unicode.IsUpper(r) {
+				name = append(name, '/')
 			}
-			path := string(name)[1:]
-			i := strings.Index(path, "/")
-			iris.Handle(strings.ToUpper(path[:i]), path[i:], handler)
+			name = append(name, unicode.ToLower(r))
 		}
+		path := string(name)[1:]
+		i := strings.Index(path, "/")
+		iris.Handle(strings.ToUpper(path[:i]), path[i:], f.Handler())
 	})
 }
 
@@ -70,6 +71,11 @@ type (
 		meta struct{} `method:"GET" path:"/hello-world" `
 	}
 	HelloGetRes struct{}
+
+	HelloPostReq struct {
+		meta struct{} `method:"POST" path:"/hello-world" `
+	}
+	HelloPostRes struct{}
 )
 
 type Hello struct{}
@@ -77,13 +83,6 @@ type Hello struct{}
 func (c *Hello) GetHelloWorld(ctx context.Context, req *HelloGetReq) (res *HelloGetRes, err error) {
 	return &HelloGetRes{}, nil
 }
-
-type (
-	HelloPostReq struct {
-		meta struct{} `method:"POST" path:"/hello-world" `
-	}
-	HelloPostRes struct{}
-)
 
 func (c *Hello) PostHelloWorld(ctx context.Context, req *HelloPostReq) (res *HelloPostRes, err error) {
 	return &HelloPostRes{}, nil
